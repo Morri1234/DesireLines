133f6f0d1586e3380326b232b809f5955b019ee3
Abhimanyu Acharya <a.acharya@spacesyntax.com>
2018-02-28 12:35:16 +0000

    Plugin builder files

---
+++ Desire_lines_dockwidget.py
@@ -0,0 +1,50 @@
+# -*- coding: utf-8 -*-
+"""
+/***************************************************************************
+ DesirelinesDockWidget
+                                 A QGIS plugin
+ Create the desire lines from a space Syntax accessibility analysis
+                             -------------------
+        begin                : 2018-02-28
+        git sha              : $Format:%H$
+        copyright            : (C) 2018 by AA/Space Syntax Limited
+        email                : a.acharya@spacesyntax.com
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+"""
+
+import os
+
+from PyQt4 import QtGui, uic
+from PyQt4.QtCore import pyqtSignal
+
+FORM_CLASS, _ = uic.loadUiType(os.path.join(
+    os.path.dirname(__file__), 'Desire_lines_dockwidget_base.ui'))
+
+
+class DesirelinesDockWidget(QtGui.QDockWidget, FORM_CLASS):
+
+    closingPlugin = pyqtSignal()
+
+    def __init__(self, parent=None):
+        """Constructor."""
+        super(DesirelinesDockWidget, self).__init__(parent)
+        # Set up the user interface from Designer.
+        # After setupUI you can access any designer object by doing
+        # self.<objectname>, and you can use autoconnect slots - see
+        # http://qt-project.org/doc/qt-4.8/designer-using-a-ui-file.html
+        # #widgets-and-dialogs-with-auto-connect
+        self.setupUi(self)
+
+    def closeEvent(self, event):
+        self.closingPlugin.emit()
+        event.accept()
+
